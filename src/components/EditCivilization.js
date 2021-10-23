9b8e3b5ea52e32699c38e2c34abd2ab6618c31c4
Tyler Vannoy <61971482+TylerJVannoy@users.noreply.github.com>
2021-10-23 14:11:00 -0700

    fixing db

--- src/components/EditCivilization.js
+++ src/components/EditCivilization.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import axios from "axios";
 import Navbar from "./Navbar";
 
-//add some random comment
 export default class EditCivilization extends Component {
   constructor(props) {
     super(props);
